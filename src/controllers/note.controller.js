219283291c5eeafa1df5d7fd54aebb42fdae517c
Diego Armando Carreño Serrano <diegocs2410@gmail.com>
2021-10-27 09:26:15 -0500

    save note and ListNoteByid ok

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -9,5 +9,32 @@ noteCtrl.listNotes = async (req, res) => {
     res.status(400).json({ ok: false, message: error.message });
   }
 };
-
+noteCtrl.listNoteById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const note = await noteModel.findById({ _id: id });
+    if (!note) {
+      return res.status(400).json({ ok: false, message: 'Note not found' });
+    }
+    res.status(201).json({ ok: true, note });
+  } catch (error) {
+    res.status(500).json({ ok: false, message: error.message });
+  }
+};
+noteCtrl.addNote = async (req, res) => {
+  try {
+    const { title, description, date, user, priority } = req.body;
+    const newNote = noteModel({
+      title,
+      description,
+      date,
+      user,
+      priority,
+    });
+    await newNote.save(); //Use save method from instance noteModel  */
+    res.status(201).json({ ok: true, message: 'Note added succesfully' });
+  } catch (error) {
+    res.status(500).json({ ok: false, message: error.message });
+  }
+};
 module.exports = noteCtrl;
